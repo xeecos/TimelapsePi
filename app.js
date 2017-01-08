37ffddcb80465b9c6467bf314cfa1b66bafceb6a
sky <sky@skydeMacBook-Pro.local>
2017-01-08 20:07:28 +0800

    update

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 const Promise = require('promise');
 const Camera = require('./libs/camera')
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('web'));
-
+app.get("/format",function(req,res){
+  Camera.sharedObject().format(req.query.exp,req.query.gain).then(function(){
+    res.send('ok');
+  })
+})
 app.listen(8000, function () {
   console.log('app listening on port 8000!');
 });
